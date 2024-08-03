62acfa9bdef320cc074b2029e690e3afb97d7f43
Taceke <tajerkemer46031@gmail.com>
2024-08-03 20:32:27 +0300

    Header and Footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <div className="App">
-      <h1>hello guys</h1>
+      <Home />
     </div>
-  );
+  ); 
 }
 
 export default App;
