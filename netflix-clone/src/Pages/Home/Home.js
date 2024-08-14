f35c21ae58d69eae49ad0e409e3ed92ddbedd2e2
Taceke <tajerkemer46031@gmail.com>
2024-08-14 04:09:11 +0300

    Banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Header/Footer/Footer'
+import Banner from '../../components/Header/Banner/Banner'
 
 const home = () => {
   return (
     <> 
         <Header/>
+        <Banner/>
         <Footer/>
     </>
   )
